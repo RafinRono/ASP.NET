87d86c46e0b249fd498ddb46d21a969e3c0eaa1f
Rono <22-47226-1@student.aiub.edu>
2025-03-05 21:21:58 +0600

    added class 2nd day file

---
+++ class/intro.NET/intro.NET/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace intro.NET.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Test()
+        {
+            var a = 10;
+            dynamic b;
+            b = 10;
+
+            ViewBag.Name = "Rono";
+            ViewBag.ID = 1;
+
+            ViewData["Name1"] = "Abrar";
+            ViewData["ID1"] = 2;
+
+            return View();
+        }
+
+        public ActionResult Redirecting()
+        {
+            TempData["Msg"] = "Redirected from Redirecting";
+            return RedirectToAction("Test");
+        }
+
+        public ActionResult Goto()
+        {
+            return Redirect("https://www.aiub.edu/");
+        }
+
+        public ActionResult newHome()
+        {
+            return View();
+        }
+    }
+}
---
+++ class/intro.NET/intro.NET/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="intro.NET.MvcApplication" Language="C#" %>
