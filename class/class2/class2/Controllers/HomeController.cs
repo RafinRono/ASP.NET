ab16884f990e853ec3f2b0f3449d51d8114e00b4
Rono <22-47226-1@student.aiub.edu>
2025-03-14 20:31:07 +0600

    added class/class2 - practicing what was taught in class (2)

---
+++ class/class2/class2/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using class2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace class2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact(Student s)
+        {
+            ViewBag.Message = "Your contact page.";
+            return View(s);
+        }
+
+        //public ActionResult Info(FormCollection fc) // using FormCollection
+        //{
+        //    ViewBag.Name = Request["Name"];
+        //    ViewBag.Id = fc["Id"];
+        //    return View();
+        //}
+
+        [HttpGet]
+        public ActionResult Info() 
+        {
+            return View(new Student());
+        }
+
+        [HttpPost]
+        //public ActionResult Info(string Name, string Id) //using variable mapping (name and type must be same on view and controller)
+        //{
+        //    ViewBag.Name = Request["Name"];
+        //    ViewBag.Id = Id;
+        //    return View();
+        //}
+        public ActionResult Info(FormCollection fc, string Email, Student s)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Contact", "Home", s);
+            }
+
+            ViewBag.Id = fc["Id"];
+            ViewBag.Email = Email;
+            return View(s);
+        }
+    }
+}
---
+++ class/class2/class2/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+
+namespace class2.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage ="You must fill this in, my brother/sister"), StringLength(10)]
+        public string Name { get; set; }
+
+        [Range(0,40)]
+        public int Id { get; set; }
+
+        [Required, StringLength(15), EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+    }
+}
