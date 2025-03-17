be530d8e5c72f69e9d5fbb19f88121e1118e9122
Rono <22-47226-1@student.aiub.edu>
2025-03-17 19:29:08 +0600

    added task_2

---
+++ task/Task_2/Lab_3/Lab_3/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab_3.Models;
+
+
+namespace Lab_3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Info()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View(new Student());
+        }
+
+        [HttpPost]
+        public ActionResult Info(Student s)
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View(s);
+        }
+    }
+}
---
+++ task/Task_2/Lab_3/Lab_3/Models/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+
+namespace Lab_3.Models
+{
+    sealed public class validateDOB : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            if (value == null)
+            {
+                return false; 
+            }
+
+            if (DateTime.TryParse(value.ToString(), out DateTime dateValue))
+            {
+                if ((DateTime.Now - dateValue).TotalDays >= 18 * 365.25)
+                {
+                    return true; 
+                }
+                return false;
+            }
+
+            return false;
+        }
+    }
+
+    sealed public class validateIdEmail : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var student = validationContext.ObjectInstance as Student;
+            if (student == null || value == null)
+            {
+                return new ValidationResult(ErrorMessage = "Id and Email-Id do Not match") { };
+            }
+
+            var emailIdPortion = student.Email.Split('@')[0];
+
+            if (student.Id != emailIdPortion)
+            {
+                return new ValidationResult(ErrorMessage = "Id and Email-Id do Not match") { };
+            }
+            return ValidationResult.Success; 
+        }
+    }
+
+
+    public class Student
+    {
+        [Required]
+        [RegularExpression(@"^[a-zA-Z.-]+$", ErrorMessage = "Name only contains alphabets, . (dot) and - (dash)")]
+        public string Name { get; set; }
+
+        [RegularExpression(@"^\d{2}-\d{5}-[1-3]$", ErrorMessage = "XX-XXXXX-X format with only digits and last digit must be between 1-3")]
+        public string Id { get; set; }
+        [validateDOB(ErrorMessage = "Must be atleast 18 years of age")]
+        public string DOB { get; set; }
+        [RegularExpression(@"^\d{2}-\d{5}-[1-3]@student.aiub.edu$", ErrorMessage = "Must be in this format: XX-XXXXX-X@student.aiub.edu")]
+        [validateIdEmail(ErrorMessage ="test")]
+        public string Email { get; set; }
+    }
+}
